bfb32eb3779751cd3cfc03c38aaa50a040556495
Yu Li <contact+github@yuli.se>
2025-08-17 14:38:28 -0400

    fix: Update test name argument to match exact pattern in JestRunner

--- src/jestRunner.ts
+++ src/jestRunner.ts
@@ -244,7 +244,7 @@ export class JestRunner {
 
     if (testName) {
       args.push('-t');
-      args.push(quoter(escapeSingleQuotes(testName)));
+      args.push(quoter(`^${escapeSingleQuotes(testName)}$`));
     }
 
     const setOptions = new Set(options);
